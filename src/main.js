5c566c3c8194f4487513c9ed6e24248c212d913c
@KatKode <sabrinaacm@gmail.com>
2019-06-27 11:01:37 -0500

    closes #13,closes #12, closes #11, closes #6, closes #5, closes #4

--- src/main.js
+++ src/main.js
@@ -11,26 +11,23 @@ const loginAccess = () => {
   if (email === 'LABORATORIA' && password === 'LABORATORIA') {
     formLogin.classList.add('hide');
     btnLogin.classList.add('hide');
+    document.body.style.backgroundImage = 'url("img/fondop3.jpg")';
     displayData();
   } else {
     message.classList.remove('hide');
     message.innerHTML = 'Usuario o password incorrectos';
-  }
+  };
 };
-
 const displayData = () => {
   let allPokemons = 0;
   for (let i = 0; i < objectPokemon.length; i++) {
     const pokemon = objectPokemon[i];
-    
     if (pokemon.multipliers === null) {
       displayPokemon.innerHTML += `
       <figure class="wp-caption">
       <input name="${pokemon.name}" type="image" src="${pokemon.img}" alt="${pokemon.name}">
       <figcaption class="wp-caption-text">${pokemon.name} 0</figcaption>
-      </figure>
-    
-    `;
+      </figure>`;
     } else {
       displayPokemon.innerHTML += `
       <figure class="wp-caption">
@@ -40,8 +37,8 @@ const displayData = () => {
       allPokemons += pokemon.multipliers.length;
       document.getElementById('header').innerHTML = `
       <h4>Total atrapados: ${allPokemons}</h4>`;
-    }
-  }
+    };
+  };
 };
 
 btnLogin.addEventListener('click', loginAccess);
